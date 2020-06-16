4f847e7a7fc1eae689266f8c34ba01feada02c02
Andy.wei <Andy.wei@higgstar.com>
2020-06-16 18:25:09 +0800

    not finished

---
+++ lib/cruddemo.Data/rdbms/configurations/ProductConfigurations.cs
@@ -0,0 +1,18 @@
+﻿using cruddemo.Data.Poco;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cruddemo.Data.rdbms.configurations
+{
+    public class ProductConfigurations : IEntityTypeConfiguration<Product>
+    {
+        void IEntityTypeConfiguration<Product>.Configure(
+            EntityTypeBuilder<Product> builder)
+        {
+            //builder.HasKey(p => new { p.Id }).IsClustered;
+        }
+    }
+}
---
+++ lib/cruddemo.Data/rdbms/dbContext/ProductContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cruddemo.Data.rdbms.dbContext
+{
+    class ProductContext
+    {
+    }
+}
