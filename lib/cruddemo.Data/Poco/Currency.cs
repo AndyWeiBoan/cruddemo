894ccda5342bcffe33ec16d75ab73e10fd7a36e6
Andy.wei <Andy.wei@higgstar.com>
2020-06-16 18:04:33 +0800

    加入專案檔案。

---
+++ lib/cruddemo.Data/Poco/Currency.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cruddemo.Data.Poco
+{
+    public enum Currency
+    {
+        CNY,
+        TWD,
+        USA
+    }
+}
---
+++ lib/cruddemo.Data/Poco/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cruddemo.Data.Poco
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTimeOffset CreateAt { get; set; }
+
+        public DateTimeOffset UpdateAt { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public string UpdatedBy { get; set; }
+
+        public Currency Currency { get; set; }
+    }
+}
